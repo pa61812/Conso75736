c7264fe05d5986b8d66fc9f5b17a912a18ebae32
pa61812 <41846770+pa61812@users.noreply.github.com>
2018-07-31 14:37:04 +0800

    99

---
+++ 99/99/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            int c;
+            while (a <= 9)
+            {
+                int b = 1;
+
+                while (b <= 9)
+                {
+                    c = a * b;
+                    Console.Write(a);
+                    Console.Write("X");
+                    Console.Write(b);
+                    Console.Write("=");
+                    if (c < 10) { Console.Write(" "); }
+                    Console.Write(c);
+                    Console.Write(" ");
+                    b++;
+                }
+                Console.WriteLine("");
+                a++;
+            }
+        }
+    }
+}

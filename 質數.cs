e4714f362bba52210867a717b1b1abe56b851020
pa61812 <41846770+pa61812@users.noreply.github.com>
2018-07-31 19:14:42 +0800

    質數

---
+++ 質數.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 質數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("開始"); Console.WriteLine();
+            Console.WriteLine();
+            int x;
+            int y;
+            bool 是質數;
+            int a;
+            Console.WriteLine("---------------------------");
+            x = 6;
+            while (x <= 15)
+            {
+                是質數 = true;
+                a = 2;
+                while ((a <= (x - 1)) && 是質數)
+                {
+                    if (x % a == 0)
+                    {
+                        是質數 = false;
+                    }
+                   a += 1;
+                }
+                if (是質數)
+                {
+                    Console.Write(x + " ");
+                }
+                x += 1;
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------------------");
+            y = 7;
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("結束");
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine();
+        }
+    
+    }
+}
